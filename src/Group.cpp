3d8c81bfe8c70a1079e9708138cad45fae4782cb
weiterins <78421207+weiterins@users.noreply.github.com>
2021-04-08 18:29:31 +0300

    Fixed conflict

--- src/Group.cpp
+++ src/Group.cpp
@@ -5,16 +5,8 @@ Group::Group(int number) : number { number }
 
 void Group::addStudent(Student& stud)
 {
-<<<<<<< HEAD
 	stud.setGroup(number);
 	students.insert(stud);
-||||||| dcfc690
-	stud.setGroup(group);
-	students.insert(std::move(stud));
-=======
-	stud.setGroup(group);
-	students.insert(stud);
->>>>>>> 766a7e5063c5050cfca3c5d41dbaf7e0018b8f55
 }
 
 bool Group::removeStudent(std::string_view name, std::string_view surname)
