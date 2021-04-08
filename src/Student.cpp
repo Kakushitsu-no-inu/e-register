79da0bdfc954b0a05d1d6f15375b3419db35b384
Kakushitsu-no-inu <andrijlasenko@gmail.com>
2021-04-08 15:29:23 +0300

    Fixed

--- src/Student.cpp
+++ src/Student.cpp
@@ -3,8 +3,8 @@
 Student::Student(std::string_view name) : Person { name }
 {}
 
-Student::Student(std::string_view name, std::string_view surname)
+Student::Student(std::string_view name, std::string_view surname) : Person { name, surname }
 {}
 
-Student::Student(std::string_view name, std::string_view surname, int group)
+Student::Student(std::string_view name, std::string_view surname, int group) : Person { name, surname }, group { group }
 {}
