ccaa63d01f3e7ac3e06d8d83064442fe74edeb3b
Kakushitsu-no-inu <andrijlasenko@gmail.com>
2021-04-08 17:40:57 +0300

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -4,4 +4,9 @@
 auto main() -> int
 {
 	Group KN_314 { 314 };
+	Student a{"Andrii","Liashenko"};
+	Student b{"Ivan","Tkachuk"};
+	KN_314.addStudent(std::move(a));
+	KN_314.addStudent(std::move(b));
+	
 }
