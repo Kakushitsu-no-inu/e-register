7402a9b77ae2175a750737d8b6fd2cebf62dc8a0
Kakushitsu-no-inu <andrijlasenko@gmail.com>
2021-04-09 16:55:19 +0300

    convet

--- include/Mark.h
+++ include/Mark.h
@@ -8,4 +8,11 @@ struct Mark
 	std::optional<int> value { std::nullopt };
 	std::string subject {};
 	std::tm date {};
+
+	std::string getDate()
+	{
+		char c[9];
+		std::strftime(c, 9, "%d/%m/%y", &date);
+		return std::string { c };
+	}
 };
