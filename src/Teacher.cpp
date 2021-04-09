1f78f1adec40608fcae9115505d021403e81b530
weiterins <78421207+weiterins@users.noreply.github.com>
2021-04-09 16:55:30 +0300

    Added current time

--- src/Teacher.cpp
+++ src/Teacher.cpp
@@ -1,4 +1,5 @@
-#include <Teacher.h>
+#include "Mark.h"
+#include "Teacher.h"
 
 Teacher::Teacher(std::string_view name, std::string_view surname) : Person(name, surname)
 {}
@@ -16,4 +17,12 @@ const std::string& Teacher::getSubject() const
 const std::string& Teacher::getPassword() const
 {
 	return password;
+}
+
+void Teacher::setMark(Student& stud, std::optional<int> value)
+{
+	std::time_t now { std::time(nullptr) };
+	std::tm* date { std::localtime(&now) };
+
+	
 }
