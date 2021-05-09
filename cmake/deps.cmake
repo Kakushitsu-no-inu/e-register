4ea6a798fd60105064140c87bd2e8dafb4779a0b
nesterpnyi <weiterins@gmail.com>
2021-05-09 15:30:18 +0300

    fetch fmt lib

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -13,15 +13,15 @@ if(NOT OpenXLSX_POPULATED)
 set(OPENXLSX_PATH ${OpenXLSX_SOURCE_DIR})
 add_subdirectory(${OPENXLSX_PATH} ${OpenXLSX_BINARY_DIR})
 
-# FetchContent_Declare(
-#     fmt
-#     GIT_REPOSITORY "https://github.com/fmtlib/fmt.git"
-#     GIT_TAG 0889856d6160775462e9e0722d121aab0e75e873
-# )
+FetchContent_Declare(
+    fmt
+    GIT_REPOSITORY "https://github.com/fmtlib/fmt.git"
+    GIT_TAG 0889856d6160775462e9e0722d121aab0e75e873
+)
 
-# FetchContent_GetProperties(fmt)
-# if(NOT fmt_POPULATED)
-#     FetchContent_Populate(fmt)
-# endif()
-# set(FMT_PATH ${fmt_SOURCE_DIR})
-# add_subdirectory(${FMT_PATH} ${fmt_BINARY_DIR})
+FetchContent_GetProperties(fmt)
+if(NOT fmt_POPULATED)
+    FetchContent_Populate(fmt)
+endif()
+set(FMT_PATH ${fmt_SOURCE_DIR})
+add_subdirectory(${FMT_PATH} ${CMAKE_BINARY_DIR}/fmt EXCLUDE_FROM_ALL)
